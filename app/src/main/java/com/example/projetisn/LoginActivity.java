5980808154bde1ae3a57993039b5cb2ee616e664
Mathis Leroy <mathis.leroy274@gmail.com>
2020-03-20 16:20:46 +0100

    login Activity designed, register Activity designed

--- app/src/main/java/com/example/projetisn/LoginActivity.java
+++ app/src/main/java/com/example/projetisn/LoginActivity.java
@@ -2,11 +2,16 @@ package com.example.projetisn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -14,6 +19,14 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button bLogin;
     private FrameLayout lAppNameLayout;
+    private EditText etEmailUsername_LoginActivity;
+
+    //onClick functions
+
+    public void onClickbLogin(View v){
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intent);
+    }
 
     public void onClickTvRegister(View v){
         Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
@@ -27,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
 
         bLogin = findViewById(R.id.bLogin);
         lAppNameLayout = findViewById(R.id.lAppThem_FrameLayout_LoginActivity);
+        etEmailUsername_LoginActivity = findViewById(R.id.et_login_username);
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -34,12 +48,6 @@ public class LoginActivity extends AppCompatActivity {
         int width = displayMetrics.widthPixels;
 
         lAppNameLayout.getLayoutParams().height = height/3;
-        bLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
     }
 }
--- app/src/main/java/com/example/projetisn/RegisterActivity.java
+++ app/src/main/java/com/example/projetisn/RegisterActivity.java
@@ -3,12 +3,26 @@ package com.example.projetisn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private LinearLayout lLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        lLogo = findViewById(R.id.lAppThem_FrameLayout_RegisterActivity);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        ViewGroup.LayoutParams layoutParams = lLogo.getLayoutParams();
+        layoutParams.height = height/3;
     }
 }
